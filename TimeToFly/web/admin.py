e757ef60317dc23c6b0f34c7e36b337d04ee3dad
ilkoTheTiger <ilkothetiger@gmail.com>
2022-04-12 13:43:07 +0300

    Upgraded wheel

--- TimeToFly/web/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from TimeToFly.web.models import Flight, Passenger, Town
-
-
-admin.site.register(Flight)
-admin.site.register(Passenger)
-admin.site.register(Town)
